7d013d4423406c3ef538c0326661ca4fbe195d7a
Jenny <han.jennyjh@outlook.com>
2021-04-03 07:21:29 +0000

    Added more useEffect functions

--- hooks-pt1.jsx
+++ hooks-pt1.jsx
@@ -13,16 +13,30 @@ export default function Greetings(props) {
 
 	useEffect(() =>{
 	  document.title = name + ' ' + surname;
-	})
-//UseEffect to change the title of the webpage automatically!!! 
+	});
+
+  //UseEffect to change the title of the webpage automatically!!! 
+  const [width, setWidth] = useState(window.innerWidth);
+  useEffect(()=> {
+    const handleResize = () => setWidth()
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  });
+  //UseEffect to change the width of the handler~~~ 
+  //Note that return can also clean up the first part of the effect
+
+
+
 
 	function handleNameChange(input) {
 		setName(input.target.value);
-	}
+	};
 
 	function handleSurnameChange(input) {
 		setSurname(input.target.value);
-	}
+	};
 
 	return (
 		<section classname={theme}>
@@ -45,6 +59,12 @@ export default function Greetings(props) {
 		    {locale}
 		    /> 
 		  </Row>
+
+      <Row label='width'>
+		  <input
+		    {width}
+		    /> 
+		  </Row>
 		</section>
-	)
+	);
 } 
